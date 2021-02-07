35ceb3d33bfa710172008c16961abe970ad05096
GyuSeok Nam <52943549+nks0614@users.noreply.github.com>
2021-02-07 14:46:52 +0900

    [ADD] Button Component

--- SampleApp/Component/Button.js
+++ SampleApp/Component/Button.js
@@ -8,8 +8,14 @@
 
 import React, {useState} from 'react';
 import {
-  StyleSheet, TextInput, View, Text, Button
+  StyleSheet, 
+  View, 
+  Text, 
+  ActivityIndicator, 
+  TouchableOpacity,
+  Image
 } from 'react-native';
+import CustomTextInput from './TextInput';
 
 const colors = {
     background : '#e3e3e3',
@@ -20,25 +26,68 @@ const colors = {
     paleGray : 'rgb(213, 227, 234)'
   }
 
-const CustomTextInput = (props) => {
+const CustomButton = (props) => {
     const [value, setValue] = useState({
 
 	})
 
 
+    if(!!props.isDisabled){
+        return(
+            <View style={props.containerStyle}>
+                <View style={props.disabledStyle}>
+                    <Text style={props.textStyle}>
+                        {props.children}
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+
+    if(!!props.isLoading){
+        return (
+            <View style={props.containerStyle}>
+                <View style={props.style}>
+                    <ActivityIndicator size='small' color={props.indicatorColor}/> 
+                </View>
+            </View>
+        )
+    }
+
     return (
-        <View style={[
-            props.styles
-            ]}>
-            <Button>
-                {props.text}
-            </Button>
+        <View style={props.containerStyle}>
+            <TouchableOpacity
+                onPress={props.onPress}
+            >
+                <View style={props.style}>
+                    {
+                        props.imgLeftStyle ?
+                        <Image style={props.imgLeftStyle}
+                            source={props.imgLeftSrc}/>
+                        : <View />
+                    }
+                    <Text style={props.textStyle}>{props.children}</Text>
+                </View>
+           </TouchableOpacity>
         </View>
-    );
+    )
 };
 
+CustomButton.defaultProps = {
+    containerStyle : styles.container,
+    isLoading : false,
+    isDisabled : false,
+    style : styles.btn,
+    textStyle : styles.text,
+    imgLeftStyle : styles.imgLeft,
+    indicatorColor : 'white'
+
+}
+
 const styles = StyleSheet.create({
-    
+    container:{
+        flex: 1
+    }
 });
 
-export default ;
+export default CustomButton;
